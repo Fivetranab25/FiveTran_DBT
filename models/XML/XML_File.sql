f01829d49540052834086a7d986267aaeb62f15c
Ankit.Bhadauria@ibm.com <Ankit.Bhadauria@ibm.com>
2025-04-21 11:02:24 +0000

    commit

---
+++ models/XML/XML_File.sql
@@ -0,0 +1,9 @@
+SELECT
+XMLGET(s.value, 'Id'):"$"::int as "ID",
+XMLGET(s.value, 'Name'):"$"::string as "Name",
+XMLGET(s.value, 'Email'):"$"::string as "Email",
+XMLGET(s.value, 'Age'):"$"::int as "Age",
+XMLGET(s.value, 'City'):"$"::string as "City"
+FROM MYDB.XML.XML_TABLE,
+LATERAL FLATTEN(TO_ARRAY(MYDB.XML.XML_TABLE._DATA:"$")) s
+  
